c22277f3a7a2044997d3c5023bbe86c5f07e3300
Liam <liam@Mac-mini-Liam.local>
2025-12-29 12:20:43 +0700

    Add function and app modules

---
+++ function/connect/config.ini.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+    'server' => getenv('DB_HOST') ?: 'localhost',
+    'username' => getenv('DB_USER') ?: 'root',
+    'password' => getenv('DB_PASS') ?: '',
+    'database' => getenv('DB_NAME') ?: 'billviet',
+];
